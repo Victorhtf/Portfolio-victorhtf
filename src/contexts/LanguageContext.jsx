bbdab9f19c9f6897e38c69378d650379bd32454b
Victorhtf <victorformisano10@gmail.com>
2023-06-28 23:02:50 -0300

    refactor: changed english to default language

--- src/contexts/LanguageContext.jsx
+++ src/contexts/LanguageContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useState } from "react";
 export const LanguageContext = createContext()
 
 export function LanguageProvider({ children }) {
-    const [language, setLanguage] = useState('english')
+    const [language, setLanguage] = useState('portuguese')
 
     const toggleLanguage = () => {
         setLanguage(language === 'english' ? 'portuguese' : 'english')
