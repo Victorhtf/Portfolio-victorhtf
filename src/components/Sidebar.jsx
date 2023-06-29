129c8ff491b83b9a68f8c79ac76272ae11814851
Victorhtf <victorformisano10@gmail.com>
2023-06-29 00:12:08 -0300

    feat: add english resume feature

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,25 +2,27 @@ import { useContext } from "react";
 import SocialNetworks from "./SocialNetworks";
 import InformationContainer from "./InformationContainer";
 import { LanguageContext } from "../contexts/LanguageContext";
-import data from "../data/data.json"
-import resume from "../resources/resume.pdf"
+import data from "../data/data.json";
+import resumePT from "../resources/resumePT.pdf";
+import resumeEN from "../resources/resumeEN.pdf";
 
 import Photo from "../img/photo.jpg";
 
-import "../styles/components/sidebar.sass"
+import "../styles/components/sidebar.sass";
 
 const Sidebar = () => {
   const { language } = useContext(LanguageContext);
+  const resumeLink = language === 'english' ? resumePT : resumeEN;
+
   return ( 
     <aside id="sidebar">
-      <img src={Photo} alt="Victor Formisano"></img>
+      <img src={Photo} alt="Victor Formisano" />
       <p className="title">{language === 'english' ? data['sidebar-ocupation-PT'] : data['sidebar-ocupation-EN']}</p>
-      <SocialNetworks/>
-      <InformationContainer/>
-      <a href={resume} className='btn'>{language === 'english' ? data['sidebar-resume-PT'] : data['sidebar-resume-EN']}</a>
-  </aside>
-
+      <SocialNetworks />
+      <InformationContainer />
+      <a href={resumeLink} className='btn'>{language === 'english' ? data['sidebar-resume-PT'] : data['sidebar-resume-EN']}</a>
+    </aside>
   );
-}
+};
 
 export default Sidebar;
