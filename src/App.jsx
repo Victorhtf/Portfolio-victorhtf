9a4e118cb3c925452bab04e7bff6ce9064dd83d7
Victorhtf <victorformisano10@gmail.com>
2023-07-04 22:58:15 -0300

    refactor: styles changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import Sidebar from './components/Sidebar.jsx'
 import MainContent from "./components/MainContent.jsx"
 import Header from './components/Header.jsx';
 
+import { useContext } from 'react';
+import { ThemeContext } from './contexts/ColorModeContext.jsx';
+
 import "./styles/components/dark-mode/app.sass"
 import "./styles/components/light-mode/app.sass"
 
-
 function App() {
+  const { theme } = useContext(ThemeContext)
   return (
-    <div id="portfolio">
+    <div className={`portfolio ${theme}`}>
       <div className="header">
         <Header/>
       </div>
